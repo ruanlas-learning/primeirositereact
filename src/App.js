7e8702ff0702fca2bb9c87647dd6c5e3f520ebaf
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-28 16:05:04 -0300

    ciclo de vida dos componentes

--- src/App.js
+++ src/App.js
@@ -5,50 +5,24 @@ class App extends Component{
     constructor(props){
         super(props);
         this.state = {
-            nome: 'Matheus',
-            contador: 0
+            hora: '00:00:00'
         };
-
-        this.aumentar = this.aumentar.bind(this);
-        this.diminuir = this.diminuir.bind(this);
-        this.mudarNome = this.mudarNome.bind(this);
     }
 
-    aumentar(){
-        console.log('Aumentou');
-        let state = this.state;
-        state.contador += 1;
-        state.nome = 'Jose';
-        this.setState(state);
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({ hora: new Date().toLocaleTimeString() });
+        }, 1000);
     }
 
-    diminuir(){
-        console.log('Diminuiu');
-        let state = this.state;
-        if(state.contador === 0){
-            alert('Opa, chegou a zero!');
-            return;
-        }
-        state.contador -= 1;
-        this.setState(state);
-    }
-
-    mudarNome(){
-        let state = this.state;
-        state.nome = prompt('Qual o novo nome?');
-        this.setState(state);
+    componentDidUpdate(){
+        console.log("Atualizou!!");
     }
 
     render(){
         return(
             <div>
-                <h1>Contador</h1>
-                {this.state.nome}  <button onClick={this.mudarNome}>Mudar o nome</button>
-                <h3>
-                    <button onClick={this.diminuir}>-</button>
-                    {this.state.contador}
-                    <button onClick={this.aumentar}>+</button>
-                </h3>
+                <h1>Meu projeto {this.state.hora}</h1>
             </div>
         );
     }
