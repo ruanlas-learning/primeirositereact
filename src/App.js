677b526a982c3f2a99bd701a0779d8f421f7ed63
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-28 16:51:03 -0300

    Eventos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, {Component} from 'react';
+import Membro from './componentes/Membro';
 
 class App extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            hora: '00:00:00'
-        };
-    }
-
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState({ hora: new Date().toLocaleTimeString() });
-        }, 1000);
-    }
-
-    componentDidUpdate(){
-        console.log("Atualizou!!");
-    }
 
     render(){
         return(
             <div>
-                <h1>Meu projeto {this.state.hora}</h1>
+                <Membro nome="Visitante"/>
             </div>
         );
     }
