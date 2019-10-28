1e1a58c7c476c0149bd4ab5df63afa0a0ad36611
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-28 14:20:21 -0300

    trabalhando com props e components e criando components especificos - pt 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 // const Bemvindo = (props) => <h2>Bem vindo(a) {props.nome}</h2>;
-const Bemvindo = (props) => {
+const Equipe = (props) => {
     return(
         <div>
-            <h2>Bem vindo(a) {props.nome}</h2>
-            <h3>Tenho {props.idade} Anos</h3>
+            <Sobre nome={props.nome} cargo={props.cargo} 
+                idade={props.idade}/>
+                <Social fb={props.facebook} />
+        </div>
+    );
+};
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <h3>Cargo(a): {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+            <hr/>
+        </div>
+    );
+};
+
+const Social = (props) => {
+    return (
+        <div>
+            <a href={props.fb}>Facebook </a>
+            <a>LinkedIn </a>
+            <a>Youtube </a>
         </div>
     );
 };
@@ -14,10 +36,10 @@ function App(){
     // return <h1>Social</h1>
     return(
         <div>
-            Olá Mundo!
-            <Bemvindo nome="Daniel" idade="23"/>
-            <Bemvindo nome="JOAO" idade="18"/>
-            <h1>Curso React</h1>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome="Lucas" cargo="Programador" idade="29" facebook="https://google.com" />
+            <Equipe nome="Matheus" cargo="Designer" idade="19" facebook="https://google.com"/>
+            <Equipe nome="Amanda" cargo="Front-end" idade="15" facebook="https://google.com"/>
         </div>
     );
 }
