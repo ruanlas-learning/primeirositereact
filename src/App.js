5f6de53d05827db2f316531774190470fc55a3fe
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-28 11:44:06 -0300

    trabalhando com props e component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+// const Bemvindo = (props) => <h2>Bem vindo(a) {props.nome}</h2>;
+const Bemvindo = (props) => {
+    return(
+        <div>
+            <h2>Bem vindo(a) {props.nome}</h2>
+            <h3>Tenho {props.idade} Anos</h3>
+        </div>
+    );
+};
+
 function App(){
     // return <h1>Social</h1>
     return(
         <div>
-            <h1>Olá mundo!</h1>
+            Olá Mundo!
+            <Bemvindo nome="Daniel" idade="23"/>
+            <Bemvindo nome="JOAO" idade="18"/>
+            <h1>Curso React</h1>
         </div>
     );
 }
